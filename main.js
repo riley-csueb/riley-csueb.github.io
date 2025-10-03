6c06082a0523903ee27756eb979bf1cd03fbd42b
Riley Fischer <rfmineguy@gmail.com>
2025-10-02 20:08:45 -0700

    [main] translate the html code we have for card views into javascript so we can generate the dom for our book entries

--- main.js
+++ main.js
@@ -23,3 +23,43 @@ function setupEventListeners() {
         });
     }
 }
+
+/*
+ * conf format
+ * {
+ *   "name": "bookname"
+ *   "coverurl": "url"
+ * }
+ */
+function createBookEntryDOM(conf) {
+    const parent_ = document.createElement('div')
+    parent_.classList.add('book-entry')
+
+    // main-block dom
+    const main_block_ = document.createElement('div')
+    main_block_.classList.add('main-block')
+    // const main_block_info = document.createElement('div')
+    // main_block_info.classList.add('info')
+    const main_block_cover = document.createElement('img')
+    main_block_cover.classList.add('cover')
+    main_block_cover.setAttribute('src', conf.coverurl)
+
+    // main_block_.appendChild(main_block_info);
+    main_block_.appendChild(main_block_cover);
+
+    // info-block dom
+    const info_block_ = document.createElement('div')
+    info_block_.classList.add('info-block')
+    const info_block_title = document.createElement('h3')
+    info_block_title.innerText = conf.name;
+    const info_block_author = document.createElement('h4')
+    info_block_author.innerText = conf.author;
+
+    info_block_.appendChild(info_block_title);
+    info_block_.appendChild(info_block_author);
+
+    parent_.appendChild(main_block_);
+    parent_.appendChild(info_block_);
+
+    return parent_;
+}
