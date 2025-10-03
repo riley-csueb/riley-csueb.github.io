104b5c6cbd26dcb96e6522e89a8b4ea8b7e01324
Riley Fischer <rfmineguy@gmail.com>
2025-10-02 20:07:55 -0700

    [main] this code in conjunction with the css makes the card views expand/fold when clicked

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-let entries = document.body.querySelectorAll('.book-entry')
-for (let entry of entries) {
-    entry.addEventListener('click', (event) => {
-        entry.classList.toggle('info')
-        const display = entry.querySelector('.info-block').style.display
-        entry.querySelector('.info-block').style.display = display === 'block' ? 'none' : 'block'
-    });
+function setupEventListeners() {
+    let entries = document.body.querySelectorAll('.book-entry')
+    console.log(entries)
+    for (let entry of entries) {
+        entry.addEventListener('click', (event) => {
+            // fold other open dom
+            let open = [...entries].filter(entry => entry.querySelector('.info-block').style.display === 'block')
+            for (let i of open) {
+                if (i == entry) continue;
+                i.querySelector('.info-block').style.display = 'none'
+                i.classList.remove('info')
+            }
+
+            // toggle this dom
+            entry.classList.toggle('info')
+            const display = entry.querySelector('.info-block').style.display
+            if (display === 'block') {
+                entry.querySelector('.info-block').style.display = 'none'
+            }
+            else {
+                entry.querySelector('.info-block').style.display = 'block'
+            }
+        });
+    }
 }
