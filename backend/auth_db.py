2b42aebd41ee4c8bc9f92d7bbe935b0c5c5bbc8b
Riley Fischer <rfmineguy@gmail.com>
2025-10-30 18:55:07 -0700

    [Backend] add login/logout functionality

--- backend/auth_db.py
+++ backend/auth_db.py
@@ -8,6 +8,7 @@ class AuthDb:
     def __init(self):
         self.dbfile = None
         self.db = {}
+        self.loggedin = []
 
     def load(self, dbfile: str):
         self.dbfile = dbfile
@@ -18,9 +19,25 @@ class AuthDb:
             print(f"File {dbfile}, not found")
         except json.JSONDecodeError:
             print("Failed to decode json")
-    
-    def check_auth(self, username: str, password: str) -> bool:
+
+    def check_auth(self, username: str, password: str) -> object:
         entry = list(filter(lambda user: user['username'] == username, self.db))
         if len(entry) == 1 and entry[0]['password'] == password:
-            return { 'status': 'ok', 'username': username }
+            return { 'status': 'ok', 'username': username, 'books': entry[0]['books']}
         return { 'status': 'fail' }
+
+    def login(self, username: str, password: str) -> object:
+        if self.isloggedin(username):
+            return { 'status': 'error', 'reason': f'User {username} already logged in' }
+        result = self.check_auth(username, password)
+        if result['status'] == 'fail':
+            return result
+        self.loggedin.append(username)
+        return result
+
+    def logout(self, username: str) -> object:
+        if not self.isloggedin(username):
+            print("Something is very wrong. A user tried to log out that never logged in")
+            return { 'status': 'error', 'reason': f'User {username} tried to log out, but they never logged in' }
+        self.loggedin.remove(username)
+        return { 'status': 'ok' }
